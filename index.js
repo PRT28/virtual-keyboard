77f26f2ba77118ac62b2b8b2a80269ebfe355ddb
PRT28 <56737649+PRT28@users.noreply.github.com>
2021-12-16 08:35:55 +0530

    updated index.js for sound

--- index.js
+++ index.js
@@ -25,18 +25,28 @@ for(let i=0; i<keys.length; ++i){
     keys[i].setAttribute('val',keys[i].innerHTML.toLowerCase());
     keys[i].addEventListener("click",function(e){
         if(keys[i].getAttribute("val")=="backspace" || keys[i].getAttribute("val")=="backspace".toUpperCase()){
+            var a=new Audio("sound.mp3");
+            a.play();
             text.value=text.value.substring(0,text.value.length-1);
         }
         else if(keys[i].getAttribute("val")=="space"|| keys[i].getAttribute("val")=="space".toUpperCase()){
+            var a=new Audio("sound.mp3");
+            a.play();
             text.value=text.value+" ";
         }
         else if(keys[i].getAttribute("val")=="enter"||keys[i].getAttribute("val")=="enter".toUpperCase()){
+            var a=new Audio("sound.mp3");
+            a.play();
             text.value=text.value+'\n';
         }
         else if(keys[i].getAttribute("val")=="capslock"||keys[i].getAttribute("val")=="capslock".toUpperCase()){
+            var a=new Audio("sound.mp3");
+            a.play();
             caps();
         }
         else{
+            var a=new Audio("sound.mp3");
+            a.play();
             text.value=text.value+keys[i].getAttribute("val");
         } 
     });
@@ -45,18 +55,28 @@ for(let i=0; i<keys.length; ++i){
 window.addEventListener('keydown',function(e){
     for(let i=0;i<keys.length;++i){
         if(e.key== keys[i].getAttribute('val') || e.key== keys[i].getAttribute('val').toUpperCase()){
+            var a=new Audio("sound.mp3");
+            a.play();
             keys[i].classList.add("active");
         }
         if(e.code=='Backspace'){
+            var a=new Audio("sound.mp3");
+            a.play();
             back.classList.add('active');
         }
         if(e.code=='Space'){
+            var a=new Audio("sound.mp3");
+            a.play();
             space.classList.add('active');
         }
         if(e.code=='Enter'){
+            var a=new Audio("sound.mp3");
+            a.play();
             enter.classList.add('active');
         }
         if(e.code=='Capslock'){
+            var a=new Audio("sound.mp3");
+            a.play();
             cap.classList.toggle('active');
         }
     }
